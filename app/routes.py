7fecc49e3e4a72fb44f8008eff41de29f4aeec5c
Felipe Sasso <felipecsasso@gmail.com>
2019-11-27 10:24:15 -0800

    Added before_request method for utcnow

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import render_template, flash, redirect, url_for
 from flask import request
 from werkzeug.urls import url_parse
@@ -69,3 +71,10 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
